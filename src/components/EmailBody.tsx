ff401df7c67497e458432f9b02cd491f9b5ffad0
tymurpysarevych <mail@devplix.com>
2024-08-23 19:35:09 +0200

    set on click event inside shadow dom

--- src/components/EmailBody.tsx
+++ src/components/EmailBody.tsx
@@ -1,21 +1,15 @@
 import './EmailBody.css';
-import {ShadowRoot} from "./shadow-root/ShadowRoot.tsx";
+import ShadowRoot from "./shadow-root/ShadowRoot.tsx";
 
 interface EmailBodyProps {
     body: string;
 }
 
 export default function EmailBody({body}: EmailBodyProps) {
-
-    // const sanitize = (html: string) => {
-    //     return DOMPurify.sanitize(html, {SANITIZE_DOM: true});
-    //     // return (html);
-    // }
-
     const shadowContent =
         <div className="body" id="container">
             <div dangerouslySetInnerHTML={{__html: body}}/>
         </div>;
 
-    return <ShadowRoot child={shadowContent}/>
+    return <ShadowRoot child={shadowContent}/>;
 }
--- src/components/shadow-root/ShadowRoot.tsx
+++ src/components/shadow-root/ShadowRoot.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+export default function ShadowRoot({child}: { child: JSX.Element }) {
 
-export class ShadowRoot extends React.Component<{ child: JSX.Element }> {
-
-    attachShadow(host: any) {
+    const attachShadow = (host: any) => {
         if (host == null) {
             return;
         }
         host.attachShadow({mode: "open"});
         host.shadowRoot.innerHTML = host.innerHTML;
         host.innerHTML = "";
+
+        const children: Element[] = Array.from(host.shadowRoot.children);
+        if (children.length === 0 || children.length > 1) {
+            // TODO handle this case in Snacks
+            console.error("Expected exactly one child in shadow root");
+        }
+
+        children[0].querySelectorAll("a").forEach((anchor) => {
+            anchor.onclick = (event) => {
+                event.preventDefault();
+                // TODO open from rust
+            }
+        });
     }
 
-    render() {
-        return (
-            <span ref={this.attachShadow}>
-                {this.props.child}
-            </span>
-        );
-    }
 
+    return <span ref={attachShadow}>{child}</span>;
 }

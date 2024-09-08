07449a88bf773a46b7eba744848f3455ee212cf8
tymurpysarevych <mail@devplix.com>
2024-09-08 20:39:25 +0200

    rename_all = "camelCase" for GEmails

--- src-tauri/src/structs/google/email.rs
+++ src-tauri/src/structs/google/email.rs
@@ -1,33 +1,37 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct GEmail {
-    pub internalDate: String,
-    pub historyId: String,
+    pub internal_date: String,
+    pub history_id: String,
     pub id: String,
     pub snippet: String,
-    pub sizeEstimate: u32,
-    pub threadId: String,
-    pub labelIds: Vec<String>,
+    pub size_estimate: u32,
+    pub thread_id: String,
+    pub label_ids: Vec<String>,
     pub payload: Payload,
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct EmailLight {
     pub id: String,
-    pub threadId: String,
+    pub thread_id: String,
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct EmailLightResponse {
     pub messages: Vec<EmailLight>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct Payload {
-    pub mimeType: String,
+    pub mime_type: String,
     pub body: Body,
-    pub partId: String,
+    pub part_id: String,
     pub filename: String,
     pub headers: Vec<Header>,
     pub parts: Option<Vec<Part>>,
@@ -40,23 +44,26 @@ impl Payload {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct Body {
     pub size: u32,
     pub data: Option<String>,
-    pub attachmentId: Option<String>,
+    pub attachment_id: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct Header {
     pub name: String,
     pub value: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct Part {
-    pub mimeType: String,
+    pub mime_type: String,
     pub body: Body,
-    pub partId: String,
+    pub part_id: String,
     pub filename: String,
     pub headers: Vec<Header>,
     pub parts: Option<Vec<Part>>,

9faf46393c457786cc86e5c7489a4510b20e2201
tymurpysarevych <mail@devplix.com>
2024-09-13 17:26:13 +0200

    added initial setup comp and google login button

---
+++ src/components/initial-setup/InitialSetup.tsx
@@ -0,0 +1,9 @@
+import AuthGoogle from "../auth-google/AuthGoogle.tsx";
+
+export default function InitialSetup() {
+    return (
+        <div>
+            <AuthGoogle/>
+        </div>
+    );
+}

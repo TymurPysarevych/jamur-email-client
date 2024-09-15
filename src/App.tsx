ba6b6767a9eac5ff1914fb3839ccd8f0a6ca055b
tymurpysarevych <mail@devplix.com>
2024-09-15 20:21:06 +0200

    added icon lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import "./App.css";
-import EmailComponent from "./components/email/EmailComponent.tsx";
-import {RecoilRoot} from "recoil";
-import LoadingComponent from "./components/loading/LoadingComponent.tsx";
-import InitialSetup from "./components/initial-setup/InitialSetup.tsx";
+import './App.css';
+import { RecoilRoot } from 'recoil';
+import LoadingComponent from './components/loading/LoadingComponent.tsx';
+import InitialSetup from './components/initial-setup/InitialSetup.tsx';
 
 export default function App() {
-    return (
-        <RecoilRoot>
-            <div className="container">
-                <LoadingComponent/>
-                <EmailComponent/>
-                <InitialSetup/>
-            </div>
-        </RecoilRoot>
-    );
+  return (
+    <RecoilRoot>
+      <div className="container">
+        <LoadingComponent />
+        <InitialSetup />
+      </div>
+    </RecoilRoot>
+  );
 }
--- src/components/smtp-setup/SmtpSetup.tsx
+++ src/components/smtp-setup/SmtpSetup.tsx
@@ -1,63 +1,72 @@
-import "./style.css"
-import {useState} from "react";
+import './style.css';
+import { useState } from 'react';
 
 export default function SmtpSetup() {
-    const [username, setUserName] = useState<string>('')
-    const [imapHost, setImapHost] = useState<string>('')
-    const [imapPort, setImapPort] = useState<number>(993)
-    const [smtpHost, setSmtpHost] = useState<string>('')
-    const [smtpPort, setSmtpPort] = useState<number>(587)
+  const [email, setEmail] = useState<string>('');
+  const [imapHost, setImapHost] = useState<string>('');
+  const [imapPort, setImapPort] = useState<number>();
+  const [smtpHost, setSmtpHost] = useState<string>('');
+  const [smtpPort, setSmtpPort] = useState<number>();
 
-    return (
-        <div className="form-container">
-            <form>
-                <div className="form-group">
-                    <label htmlFor="username">Username/Email:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUserName(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="imapHost">IMAP Host:</label>
-                    <input
-                        type="text"
-                        id="imapHost"
-                        value={imapHost}
-                        onChange={(e) => setImapHost(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="imapPort">IMAP Port:</label>
-                    <input
-                        type="number"
-                        id="imapPort"
-                        value={imapPort}
-                        onChange={(e) => setImapPort(Number(e.target.value))}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="smtpHost">SMTP Host:</label>
-                    <input
-                        type="text"
-                        id="smtpHost"
-                        value={smtpHost}
-                        onChange={(e) => setSmtpHost(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="smtpPort">SMTP Port:</label>
-                    <input
-                        type="number"
-                        id="smtpPort"
-                        value={smtpPort}
-                        onChange={(e) => setSmtpPort(Number(e.target.value))}
-                    />
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+  return (
+    <div className="form-container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="example@mail.com"
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-    );
+        <div className="inline-group">
+          <div className="form-group">
+            <label htmlFor="imapHost">IMAP Host:</label>
+            <input
+              placeholder="imap.mail.com"
+              type="text"
+              id="imapHost"
+              value={imapHost}
+              onChange={(e) => setImapHost(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="imapPort">IMAP Port:</label>
+            <input
+              placeholder="993"
+              type="number"
+              id="imapPort"
+              value={imapPort}
+              onChange={(e) => setImapPort(Number(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="inline-group">
+          <div className="form-group">
+            <label htmlFor="smtpHost">SMTP Host:</label>
+            <input
+              placeholder="smtp.mail.com"
+              type="text"
+              id="smtpHost"
+              value={smtpHost}
+              onChange={(e) => setSmtpHost(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="smtpPort">SMTP Port:</label>
+            <input
+              placeholder="587"
+              type="number"
+              id="smtpPort"
+              value={smtpPort}
+              onChange={(e) => setSmtpPort(Number(e.target.value))}
+            />
+          </div>
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
 }
